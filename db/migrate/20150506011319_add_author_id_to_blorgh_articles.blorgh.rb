8dbe8372da52c61a9c18ff6048bd0dafae126727
Gregory Hodum <ghodum@yahoo.com>
2016-05-30 21:20:17 -0400

    simplified example

--- db/migrate/20150506011319_add_author_id_to_blorgh_articles.blorgh.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from blorgh (originally 20150506011232)
-class AddAuthorIdToBlorghArticles < ActiveRecord::Migration
-  def change
-    add_column :blorgh_articles, :author_id, :integer
-  end
-end
